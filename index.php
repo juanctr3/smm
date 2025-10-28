81ca0a6f0ef082ac104f6da3ed75a4ba0ca07024
Juan Carlos <71723322+juanctr3@users.noreply.github.com>
2025-10-27 19:18:34 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
 
+<link rel="manifest" href="/manifest.json">
+<script>
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+          .then(registration => {
+            console.log('ServiceWorker registrado con éxito:', registration);
+            // Función para suscribir al usuario a Push Notifications
+            subscribeUserToPush(); 
+          })
+          .catch(error => {
+            console.error('Fallo el registro de ServiceWorker:', error);
+          });
+      });
+    }
+</script>
